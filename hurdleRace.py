40fb13545f9a88d9a069b40732dabc95a1559f78
Avani1992 <52242393+Avani1992@users.noreply.github.com>
2019-07-08 14:00:59 +0530

    Add files via upload

---
+++ hurdleRace.py
@@ -0,0 +1,24 @@
+"""
+Dan is playing a video game in which his character competes in a hurdle race. Hurdles are of varying heights, and 
+Dan has a maximum height he can jump. There is a magic potion he can take that will increase his maximum height by 1 
+unit for each dose. How many doses of the potion must he take to be able to jump all of the hurdles.
+
+Given an array of hurdle heights height, and an initial maximum height Dan can jump,k , determine the minimum 
+number of doses Dan must take to be able to clear all the hurdles in the race.
+
+For example, if height=[1,2,3,3,2]  and Dan can jump 1 unit high naturally, he must take 3-1=2 doses of potion 
+to be able to jump all of the hurdles.
+"""
+height=[2,5,4,5,2]
+def hurdleRace(k, height):
+  max1=max(height)
+  if(max1>k):
+    jump=max1-k
+  else:
+    jump=0
+    
+  return jump
+
+  
+result=hurdleRace(7, height)
+print(result)
